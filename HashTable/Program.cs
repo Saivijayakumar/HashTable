9e980c35d97a5e5ca0fdd89467f55b2ad3cc5922
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-13 13:28:09 +0530

    [SaiVijayaKumar] Commiting Welcome Message

---
+++ HashTable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    class Program
+    {
+        /// <summary>
+        /// --------------------->Welcome To Hash Table<-----------------
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---------------->Welcome To Hash Table<-----------------");
+            Console.ReadLine();
+        }
+    }
+}
